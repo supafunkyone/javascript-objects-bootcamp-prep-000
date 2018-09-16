541a4cc8e004a61cbcd19d24a0e77e9ecc9c5493
alexsoleleris <alexsoleleris@gmail.com>
2018-09-16 13:07:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,4 @@ function updatePlaylist(playlist, artistName, SongTitle) {
   playlist["Phil Ochs"] = "Here's to the State of Mississippi"
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  playlist["Slowdive"]
-}
+delete artistName.Slowdive;
