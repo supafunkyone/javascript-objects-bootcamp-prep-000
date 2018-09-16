7ca8ce1a368a71bda1b7617f213d81ca4a3e4c6f
alexsoleleris <alexsoleleris@gmail.com>
2018-09-16 13:20:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -14,8 +14,9 @@ function removeFromPlaylist(playlist, artist) {
 */
 
  const playlist = {
- song: 'I wanna  love you '
- }
+  Slowdive: "Alison",
+  MyBloodyValentine: "Sometimes"
+}
    
 // 2 objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
  const updatePlaylist = function updatePlaylist(playlist, artistName, songTitle)  {
