f61d58d02b68a4fbd34e6721371afd2510fd51c4
alexsoleleris <alexsoleleris@gmail.com>
2018-09-16 13:13:50 +0000

    automatically backed up by learn

--- test/objects-test.js
+++ test/objects-test.js
@@ -27,7 +27,7 @@ describe('objects', () => {
       removeFromPlaylist(playlist, 'Slowdive')
 
       expect(playlist).
-        to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+        to.contain.all.keys({'myBloodyValentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
 
       expect(playlist).
         not.to.have.all.keys({'Slowdive': 'Alison'})
