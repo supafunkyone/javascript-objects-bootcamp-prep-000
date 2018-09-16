b2bb533ac43380695b66fc0d09bb39bf661971e5
alexsoleleris <alexsoleleris@gmail.com>
2018-09-16 13:25:29 +0000

    automatically backed up by learn

--- test/objects-test.js
+++ test/objects-test.js
@@ -21,7 +21,20 @@ describe('objects', () => {
     expect(Object.keys(playlist).length).to.be.greaterThan(0)
   })
 
- 
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
+
   describe('removeFromPlaylist(playlist, artistName)', () => {
     it('removes `artistName` from `playlist`', () => {
       removeFromPlaylist(playlist, 'Slowdive')
