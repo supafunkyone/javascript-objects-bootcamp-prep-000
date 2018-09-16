45b4ecc1bd895b4e5c1a1257340429e680768c9f
alexsoleleris <alexsoleleris@gmail.com>
2018-09-16 13:04:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,5 +8,5 @@ function updatePlaylist(playlist, artistName, SongTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  playlist["myBloodyValentine"]
+  playlist["My Bloody Valentine"]
 }
