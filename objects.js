d9413be933c9ee4e6fa08db28292977637abb27f
alexsoleleris <alexsoleleris@gmail.com>
2018-09-16 13:03:12 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  Slowdive: "Alison",
+  myBloodyValentine: "Sometimes"
+}
+
+function updatePlaylist(playlist, artistName, SongTitle) {
+  playlist["Phil Ochs"] = "Here's to the State of Mississippi"
+}
+
+function removeFromPlaylist(playlist, artistName) {}
